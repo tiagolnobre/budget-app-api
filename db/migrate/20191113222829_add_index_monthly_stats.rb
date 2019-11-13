430a62aa6d1cfaee9fa6151f88e8788da542a09f
tiago.nobre <tiago.l.nobre@gmail.com>
2019-11-13 11:46:52 +0000

    fix migration naming

--- db/migrate/20191113222829_add_index_monthly_stats.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToMonthlyStats < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :monthly_stats, %i[account]
-    add_index :monthly_stats, %i[account month year], unique: true
-  end
-end
---
+++ db/migrate/20191113222829_add_index_to_monthly_stats.rb
@@ -0,0 +1,5 @@
+class AddIndexToMonthlyStats < ActiveRecord::Migration[5.2]
+  def change
+    add_index :monthly_stats, %i[account_id month year], unique: true
+  end
+end
