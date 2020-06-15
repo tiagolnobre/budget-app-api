837ed3e52e5f6ed7fd9edd130664fea7db670fcd
tiago.nobre <tiago.l.nobre@gmail.com>
2020-06-15 09:41:14 +0100

    UpdateCategoryMonthlyBalances

---
+++ app/jobs/update_category_monthly_balances.rb
@@ -0,0 +1,19 @@
+class UpdateCategoryMonthlyBalances < ApplicationJob
+  def perform(account:, month:, year:)
+    transactions = account.user.transactions.month(month).year(year)
+
+    transactions.pluck(:category).uniq.each do |category|
+      category_transactions = transactions.category(category)
+
+      category_monthly_stat =
+        CategoryMonthlyStat.find_or_initialize_by(account: account,
+                                                  month: month,
+                                                  year: year,
+                                                  category: category)
+
+      category_monthly_stat.positive_balance = category_transactions.positive_sum
+      category_monthly_stat.negative_balance = category_transactions.negative_sum
+      category_monthly_stat.save!
+    end
+  end
+end
