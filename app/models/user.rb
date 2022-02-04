05b1dba316394c05b699d2be4f83085fff587f57
tiago <tiago.l.nobre@gmail.com>
2022-02-04 22:34:04 +0000

    Add noticed library
    - Create user welcome db notification

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,18 @@ class User < ApplicationRecord
 
   has_many :accounts, dependent: :destroy
   has_many :transactions, dependent: :destroy
+  has_many :notifications, as: :recipient, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+
+  after_create_commit :notify_recipient
+
+  private
+
+  def notify_recipient
+    NewMessageNotification.with(type: self.class.name, message_key: :new_record).deliver_later(self)
+  end
 end
