7c3cfba5afeddad933bb1da5c13fbe8cd33bf429
tiago <tiago.l.nobre@gmail.com>
2022-02-04 22:34:04 +0000

    Add noticed library
    - Create user welcome db notification

---
+++ app/notifications/new_message_notification.rb
@@ -0,0 +1,18 @@
+# Notifications::NewMessage.with(type: 'User', message_key: :new_user).deliver_later(current_user)
+#
+
+  class NewMessageNotification < Noticed::Base
+    deliver_by :database, format: :to_database
+
+    param :type, :message_key
+
+    def to_database
+      { recipient: recipient, type: self.class.name, params: params }
+    end
+
+    # I18n helpers
+    def message
+      t(".messages.#{params[:message_key]}")
+    end
+  end
+
