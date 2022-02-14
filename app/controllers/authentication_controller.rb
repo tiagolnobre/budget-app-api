ca264d76079ad7e2e736c368989b7e7a5d6354c0
tiago <tiago.l.nobre@gmail.com>
2022-02-14 17:57:04 +0000

    Fix add time duration

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -8,8 +8,7 @@ class AuthenticationController < ApplicationController
     @user = User.find_by(email: params[:email].downcase)
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
-      time = Time.zone.now + 24.hours
-      render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
+      render json: { token: token, exp: 24.hours.from_now.strftime('%m-%d-%Y %H:%M'),
                      username: @user.username }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
